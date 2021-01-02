5eefaa00bf25c0f6650a4bc4c63adf6ae304eccc
kristensew <kss0016@mix.wvu.edu>
2021-01-01 23:50:39 -0500

    added files

---
+++ slick.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+$(document).ready(function(){
+$('.your-class').slick({
+setting-name: setting-value
+});
+});
+</script>
+
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
